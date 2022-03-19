2d9e5c5ca57005ac4097138705ded487385d4fd7
João Maia <jpmmaia@gmail.com>
2022-03-19 11:36:53 +0200

    Create functions to add and remove statements

--- Source/Editor/Editor.cppm
+++ Source/Editor/Editor.cppm
@@ -1,12 +1,21 @@
 module;
 
+#include <cstdint>
+
 export module h.editor;
 
 import h.core;
 
 namespace h::editor
 {
-    void add_statement(h::Function& function);
+    export void add_statement(
+        h::Function& function,
+        h::Statement statement,
+        std::uint64_t position
+    );
 
-    void remove
+    export void remove_statement(
+        h::Function& function,
+        std::uint64_t position
+    );
 }
--- Source/Editor/Editor.tests.cpp
+++ Source/Editor/Editor.tests.cpp
@@ -2,7 +2,136 @@
 #define CATCH_CONFIG_ENABLE_ALL_STRINGMAKERS
 #include <catch2/catch.hpp>
 
-TEST_CASE("Beep beep")
+import h.core;
+import h.editor;
+
+TEST_CASE("Add/remove statement preserve variable references")
 {
-    CHECK(1 == 1);
+    using namespace h;
+
+    Function_type const function_type
+    {
+        .return_type = Type
+        {
+            .data = Integer_type
+            {
+                .precision = 64
+            }
+        },
+        .parameter_types = {}
+    };
+
+    Function function
+    {
+        .type = function_type,
+        .name = "generate",
+        .argument_names = {},
+        .linkage = Linkage::External,
+        .statements = {}
+    };
+
+    {
+        Expression expression
+        {
+            .data = Constant_expression
+            {
+                .type = Type
+                {
+                    .data = Integer_type
+                    {
+                        .precision = 64
+                    }
+                },
+                .data = Integer_constant
+                {
+                    .number_of_bits = 64,
+                    .is_signed = false,
+                    .value = 100,
+                }
+            }
+        };
+
+        Statement statement
+        {
+            .name = "var0",
+            .expressions =
+            {
+                expression
+            }
+        };
+
+        editor::add_statement(function, std::move(statement), 0);
+    }
+
+    REQUIRE(function.statements.size() == 1);
+    CHECK(std::get<Integer_constant>(std::get<Constant_expression>(function.statements[0].expressions[0].data).data).value == 100);
+
+    {
+        Expression expression
+        {
+            .data = Return_expression
+            {
+                .variable = {.type = Variable_expression::Type::Local_variable, .index = 0 },
+            }
+        };
+
+        Statement statement
+        {
+            .name = "return var0",
+            .expressions =
+            {
+                expression
+            }
+        };
+
+        editor::add_statement(function, std::move(statement), 1);
+    }
+
+    REQUIRE(function.statements.size() == 2);
+    CHECK(std::get<Integer_constant>(std::get<Constant_expression>(function.statements[0].expressions[0].data).data).value == 100);
+    CHECK(std::get<Return_expression>(function.statements[1].expressions[0].data).variable.index == 0);
+
+    {
+        Expression expression
+        {
+            .data = Constant_expression
+            {
+                .type = Type
+                {
+                    .data = Integer_type
+                    {
+                        .precision = 64
+                    }
+                },
+                .data = Integer_constant
+                {
+                    .number_of_bits = 64,
+                    .is_signed = false,
+                    .value = 200,
+                }
+            }
+        };
+
+        Statement statement
+        {
+            .name = "var1",
+            .expressions =
+            {
+                expression
+            }
+        };
+
+        editor::add_statement(function, std::move(statement), 0);
+    }
+
+    REQUIRE(function.statements.size() == 3);
+    CHECK(std::get<Integer_constant>(std::get<Constant_expression>(function.statements[0].expressions[0].data).data).value == 200);
+    CHECK(std::get<Integer_constant>(std::get<Constant_expression>(function.statements[1].expressions[0].data).data).value == 100);
+    CHECK(std::get<Return_expression>(function.statements[2].expressions[0].data).variable.index == 1);
+
+    editor::remove_statement(function, 0);
+
+    REQUIRE(function.statements.size() == 2);
+    CHECK(std::get<Integer_constant>(std::get<Constant_expression>(function.statements[0].expressions[0].data).data).value == 100);
+    CHECK(std::get<Return_expression>(function.statements[1].expressions[0].data).variable.index == 0);
 }
