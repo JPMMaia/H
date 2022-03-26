a71b8f044b6b0ceadf3570be03171352a180d270
João Maia <jpmmaia@gmail.com>
2022-03-26 18:01:39 +0200

    Add comparison and output operators to Core

---
+++ Source/Core/Core.cpp
@@ -0,0 +1,374 @@
+module;
+
+#include <ostream>
+#include <variant>
+#include <vector>
+
+module h.core;
+
+namespace h
+{
+    namespace
+    {
+        template<typename Value_type>
+        std::ostream& operator<<(std::ostream& output_stream, std::pmr::vector<Value_type> const& values)
+        {
+            output_stream << '[';
+            {
+                for (std::size_t index = 0; (index + 1) < values.size(); ++index)
+                {
+                    if constexpr (std::is_same_v<Value_type, std::pmr::string>)
+                    {
+                        output_stream << '"' << values[index] << '"' << ',';
+                    }
+                    else
+                    {
+                        output_stream << values[index] << ',';
+                    }
+                }
+
+                if (!values.empty())
+                {
+                    if constexpr (std::is_same_v<Value_type, std::pmr::string>)
+                    {
+                        output_stream << '"' << values.back() << '"';
+                    }
+                    else
+                    {
+                        output_stream << values.back();
+                    }
+                }
+            }
+            output_stream << ']';
+
+            return output_stream;
+        }
+
+        template<typename T>
+        void write(std::ostream& output_stream, char const* const name, T const& value, bool const add_comma = true)
+        {
+            if constexpr (std::is_same_v<T, std::pmr::string>)
+            {
+                output_stream << '"' << name << '"' << ": \"" << value << '"';
+
+                if (add_comma)
+                {
+                    output_stream << ',';
+                }
+            }
+            else
+            {
+                output_stream << '"' << name << '"' << ": " << value;
+
+                if (add_comma)
+                {
+                    output_stream << ',';
+                }
+            }
+        }
+    }
+
+    std::ostream& operator<<(std::ostream& output_stream, Integer_type const value)
+    {
+        output_stream << '{';
+        {
+            write(output_stream, "precision", static_cast<std::uint64_t>(value.precision), false);
+        }
+        output_stream << '}';
+
+        return output_stream;
+    }
+
+    std::ostream& operator<<(std::ostream& output_stream, Float_type const value)
+    {
+        output_stream << '{';
+        {
+            write(output_stream, "precision", static_cast<std::uint64_t>(value.precision), false);
+        }
+        output_stream << '}';
+
+        return output_stream;
+    }
+
+    std::ostream& operator<<(std::ostream& output_stream, Type const& value)
+    {
+        output_stream << '{';
+        {
+            auto const visitor = [&](auto&& arg) -> void
+            {
+                using T = std::decay_t<decltype(arg)>;
+
+                output_stream << "\"type\": \"";
+                if constexpr (std::is_same_v<T, Float_type>)
+                    output_stream << "float_type";
+                else if constexpr (std::is_same_v<T, Integer_type>)
+                    output_stream << "integer_type";
+                else
+                    static_assert(always_false_v<T>, "non-exhaustive visitor!");
+                output_stream << "\", ";
+
+                output_stream << "\"data\": " << arg;
+            };
+
+            std::visit(visitor, value.data);
+        }
+        output_stream << '}';
+
+        return output_stream;
+    }
+
+    std::ostream& operator<<(std::ostream& output_stream, Variable_expression::Type const value)
+    {
+        output_stream << '"';
+        switch (value)
+        {
+        case Variable_expression::Type::Function_argument:
+            output_stream << "function_argument";
+            break;
+        case Variable_expression::Type::Local_variable:
+            output_stream << "local_variable";
+            break;
+        case Variable_expression::Type::Temporary:
+            output_stream << "temporary";
+            break;
+        }
+        output_stream << '"';
+
+        return output_stream;
+    }
+
+    std::ostream& operator<<(std::ostream& output_stream, Variable_expression const value)
+    {
+        output_stream << '{';
+        {
+            write(output_stream, "type", value.type);
+            write(output_stream, "id", value.id, false);
+        }
+        output_stream << '}';
+
+        return output_stream;
+    }
+
+    std::ostream& operator<<(std::ostream& output_stream, Binary_expression::Operation value)
+    {
+        output_stream << '"';
+        switch (value)
+        {
+        case Binary_expression::Operation::Add:
+            output_stream << "add";
+            break;
+        case Binary_expression::Operation::Subtract:
+            output_stream << "subtract";
+            break;
+        case Binary_expression::Operation::Multiply:
+            output_stream << "multiply";
+            break;
+        case Binary_expression::Operation::Signed_divide:
+            output_stream << "signed_divide";
+            break;
+        case Binary_expression::Operation::Unsigned_divide:
+            output_stream << "unsigned_divide";
+            break;
+        case Binary_expression::Operation::Less_than:
+            output_stream << "less_than";
+            break;
+        }
+        output_stream << '"';
+
+        return output_stream;
+    }
+
+    std::ostream& operator<<(std::ostream& output_stream, Binary_expression const& value)
+    {
+        output_stream << '{';
+        {
+            write(output_stream, "left_hand_side", value.left_hand_side);
+            write(output_stream, "right_hand_side", value.right_hand_side);
+            write(output_stream, "operation", value.operation, false);
+        }
+        output_stream << '}';
+
+        return output_stream;
+    }
+
+    std::ostream& operator<<(std::ostream& output_stream, Call_expression const& value)
+    {
+        output_stream << '{';
+        {
+            write(output_stream, "function_name", value.function_name);
+            write(output_stream, "arguments", value.arguments, false);
+        }
+        output_stream << '}';
+
+        return output_stream;
+    }
+
+    std::ostream& operator<<(std::ostream& output_stream, Integer_constant const& value)
+    {
+        output_stream << '{';
+        {
+            write(output_stream, "number_of_bits", value.number_of_bits);
+            write(output_stream, "is_signed", value.is_signed);
+            write(output_stream, "value", value.value, false);
+        }
+        output_stream << '}';
+
+        return output_stream;
+    }
+
+    std::ostream& operator<<(std::ostream& output_stream, Half_constant const value)
+    {
+        output_stream << '{';
+        {
+            write(output_stream, "value", value.value, false);
+        }
+        output_stream << '}';
+
+        return output_stream;
+    }
+
+    std::ostream& operator<<(std::ostream& output_stream, Float_constant const value)
+    {
+        output_stream << '{';
+        {
+            write(output_stream, "value", value.value, false);
+        }
+        output_stream << '}';
+
+        return output_stream;
+    }
+
+    std::ostream& operator<<(std::ostream& output_stream, Double_constant const value)
+    {
+        output_stream << '{';
+        {
+            write(output_stream, "value", value.value, false);
+        }
+        output_stream << '}';
+
+        return output_stream;
+    }
+
+    std::ostream& operator<<(std::ostream& output_stream, Constant_expression const& value)
+    {
+        output_stream << '{';
+        {
+            write(output_stream, "type", value.type);
+
+            auto const visitor = [&](auto&& arg) -> void
+            {
+                output_stream << arg;
+            };
+
+            output_stream << "\"data\": ";
+            std::visit(visitor, value.data);
+        }
+        output_stream << '}';
+
+        return output_stream;
+    }
+
+    std::ostream& operator<<(std::ostream& output_stream, Return_expression const& value)
+    {
+        output_stream << '{';
+        {
+            write(output_stream, "variable", value.variable, false);
+        }
+        output_stream << '}';
+
+        return output_stream;
+    }
+
+    std::ostream& operator<<(std::ostream& output_stream, Expression const& value)
+    {
+        output_stream << '{';
+        {
+            auto const visitor = [&](auto&& arg) -> void
+            {
+                using T = std::decay_t<decltype(arg)>;
+
+                output_stream << "\"type\": \"";
+                if constexpr (std::is_same_v<T, Binary_expression>)
+                    output_stream << "binary_expression";
+                else if constexpr (std::is_same_v<T, Call_expression>)
+                    output_stream << "call_expression";
+                else if constexpr (std::is_same_v<T, Constant_expression>)
+                    output_stream << "constant_expression";
+                else if constexpr (std::is_same_v<T, Return_expression>)
+                    output_stream << "return_expression";
+                else if constexpr (std::is_same_v<T, Variable_expression>)
+                    output_stream << "variable_expression";
+                else
+                    static_assert(always_false_v<T>, "non-exhaustive visitor!");
+                output_stream << "\", ";
+
+                output_stream << "\"data\": " << arg;
+            };
+
+            std::visit(visitor, value.data);
+        }
+        output_stream << '}';
+
+        return output_stream;
+    }
+
+    std::ostream& operator<<(std::ostream& output_stream, Statement const& value)
+    {
+        output_stream << '{';
+        {
+            write(output_stream, "id", value.id);
+            write(output_stream, "name", value.name);
+            write(output_stream, "expressions", value.expressions, false);
+        }
+        output_stream << '}';
+
+        return output_stream;
+    }
+
+    std::ostream& operator<<(std::ostream& output_stream, Function_type const& value)
+    {
+        output_stream << '{';
+        {
+            write(output_stream, "return_type", value.return_type);
+            write(output_stream, "parameter_types", value.parameter_types, false);
+        }
+        output_stream << '}';
+
+        return output_stream;
+    }
+
+    std::ostream& operator<<(std::ostream& output_stream, Linkage const value)
+    {
+        output_stream << '"';
+        switch (value)
+        {
+        case Linkage::External:
+            output_stream << "external";
+            break;
+        case Linkage::Private:
+            output_stream << "private";
+            break;
+        default:
+            throw std::runtime_error{ "Linkage type is not recognized!" };
+        }
+        output_stream << '"';
+
+        return output_stream;
+    }
+
+    std::ostream& operator<<(std::ostream& output_stream, Function const& value)
+    {
+        output_stream << '{';
+        {
+            write(output_stream, "type", value.type);
+            write(output_stream, "name", value.name);
+            write(output_stream, "argument_ids", value.argument_ids);
+            write(output_stream, "argument_names", value.argument_names);
+            write(output_stream, "linkage", value.linkage);
+            write(output_stream, "statements", value.statements, false);
+        }
+        output_stream << '}';
+
+        return output_stream;
+    }
+}
--- Source/Core/Core.cppm
+++ Source/Core/Core.cppm
@@ -1,6 +1,7 @@
 module;
 
 #include <memory_resource>
+#include <ostream>
 #include <string>
 #include <unordered_map>
 #include <variant>
@@ -13,11 +14,17 @@ namespace h
     export struct Integer_type
     {
         std::uint8_t precision = 64;
+
+        friend auto operator<=>(Integer_type const&, Integer_type const&) = default;
+        friend std::ostream& operator<<(std::ostream& output_stream, Integer_type value);
     };
 
     export struct Float_type
     {
         std::uint8_t precision = 64;
+        
+        friend auto operator<=>(Float_type const&, Float_type const&) = default;
+        friend std::ostream& operator<<(std::ostream& output_stream, Float_type value);
     };
 
     export struct Type
@@ -28,6 +35,9 @@ namespace h
         >;
 
         Data_type data;
+
+        friend auto operator<=>(Type const&, Type const&) = default;
+        friend std::ostream& operator<<(std::ostream& output_stream, Type const& value);
     };
 
     export struct Variable_expression
@@ -41,6 +51,10 @@ namespace h
 
         Type type;
         std::uint64_t id;
+
+        friend auto operator<=>(Variable_expression const&, Variable_expression const&) = default;
+        friend std::ostream& operator<<(std::ostream& output_stream, Variable_expression::Type value);
+        friend std::ostream& operator<<(std::ostream& output_stream, Variable_expression value);
     };
 
     export struct Binary_expression
@@ -58,12 +72,19 @@ namespace h
         Variable_expression left_hand_side;
         Variable_expression right_hand_side;
         Operation operation;
+
+        friend auto operator<=>(Binary_expression const&, Binary_expression const&) = default;
+        friend std::ostream& operator<<(std::ostream& output_stream, Binary_expression::Operation value);
+        friend std::ostream& operator<<(std::ostream& output_stream, Binary_expression const& value);
     };
 
     export struct Call_expression
     {
         std::pmr::string function_name;
         std::pmr::vector<Variable_expression> arguments;
+
+        friend auto operator<=>(Call_expression const&, Call_expression const&) = default;
+        friend std::ostream& operator<<(std::ostream& output_stream, Call_expression const& value);
     };
 
     export struct Integer_constant
@@ -71,21 +92,33 @@ namespace h
         unsigned int number_of_bits;
         bool is_signed;
         std::uint64_t value;
+
+        friend auto operator<=>(Integer_constant const&, Integer_constant const&) = default;
+        friend std::ostream& operator<<(std::ostream& output_stream, Integer_constant const& value);
     };
 
     export struct Half_constant
     {
         float value;
+        
+        friend auto operator<=>(Half_constant const&, Half_constant const&) = default;
+        friend std::ostream& operator<<(std::ostream& output_stream, Half_constant value);
     };
 
     export struct Float_constant
     {
         float value;
+        
+        friend auto operator<=>(Float_constant const&, Float_constant const&) = default;
+        friend std::ostream& operator<<(std::ostream& output_stream, Float_constant value);
     };
 
     export struct Double_constant
     {
         double value;
+        
+        friend auto operator<=>(Double_constant const&, Double_constant const&) = default;
+        friend std::ostream& operator<<(std::ostream& output_stream, Double_constant value);
     };
 
     export struct Constant_expression
@@ -99,11 +132,17 @@ namespace h
 
         Type type;
         Data_type data;
+        
+        friend auto operator<=>(Constant_expression const&, Constant_expression const&) = default;
+        friend std::ostream& operator<<(std::ostream& output_stream, Constant_expression const& value);
     };
 
     export struct Return_expression
     {
         Variable_expression variable;
+        
+        friend auto operator<=>(Return_expression const&, Return_expression const&) = default;
+        friend std::ostream& operator<<(std::ostream& output_stream, Return_expression const& value);
     };
 
     export struct Expression
@@ -117,6 +156,9 @@ namespace h
         >;
 
         Data_type data;
+        
+        friend auto operator<=>(Expression const&, Expression const&) = default;
+        friend std::ostream& operator<<(std::ostream& output_stream, Expression const& value);
     };
 
     export struct Statement
@@ -124,12 +166,18 @@ namespace h
         std::uint64_t id;
         std::pmr::string name;
         std::pmr::vector<Expression> expressions;
+        
+        friend auto operator<=>(Statement const&, Statement const&) = default;
+        friend std::ostream& operator<<(std::ostream& output_stream, Statement const& value);
     };
 
     export struct Function_type
     {
         Type return_type;
         std::pmr::vector<Type> parameter_types;
+        
+        friend auto operator<=>(Function_type const&, Function_type const&) = default;
+        friend std::ostream& operator<<(std::ostream& output_stream, Function_type const& value);
     };
 
     export enum class Linkage
@@ -137,6 +185,8 @@ namespace h
         External,
         Private
     };
+
+    std::ostream& operator<<(std::ostream& output_stream, Linkage value);
 
     export struct Function
     {
@@ -146,5 +196,8 @@ namespace h
         std::pmr::vector<std::pmr::string> argument_names;
         Linkage linkage;
         std::pmr::vector<Statement> statements;
+        
+        friend auto operator<=>(Function const&, Function const&) = default;
+        friend std::ostream& operator<<(std::ostream& output_stream, Function const& value);
     };
 }
