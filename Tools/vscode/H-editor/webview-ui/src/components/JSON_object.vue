6022a7e501612bc40a625a09118288b4409bbc03
João Maia <jpmmaia@gmail.com>
2022-07-15 17:25:37 +0300

    Add Press_key_editable

--- Tools/vscode/H-editor/webview-ui/src/components/JSON_object.vue
+++ Tools/vscode/H-editor/webview-ui/src/components/JSON_object.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { computed, defineProps } from "vue";
 import Editable from './Editable.vue';
+import Press_key_editable from "./Press_key_editable.vue";
 
 const properties = defineProps<{
   value: any;
@@ -45,6 +46,14 @@ function on_object_content_change(key: string, childPosition: any[], value: any)
     emit("update:value", position, value);
 }
 
+function on_add_or_remove_array_element(event: KeyboardEvent, index: number): void {
+    if (event.key === "Enter") {
+        console.log("Insert at " + index);
+    }
+    else if (event.key === "Backspace") {
+        console.log("Delete at " + (index - 1));
+    }
+}
 </script>
 
 <template>
@@ -56,14 +65,19 @@ function on_object_content_change(key: string, childPosition: any[], value: any)
     </span>
     <span v-else-if="Array.isArray(properties.value)">
         <span>
-            <span>[</span>
+            <span>[<Press_key_editable placeholder="+" v-on:on_key_up="(event) => on_add_or_remove_array_element(event, 0)">
+                </Press_key_editable></span>
             <br v-if="properties.value.length !== 0">
             <span v-for="(item, index) in properties.value" v-bind:key="index">
                 <span :style="css_variables" class="key_indent">
                     <JSON_object :value="item"
                         v-on:update:value="(position, value) => on_array_content_change(index, position, value)"
                         :indentation=children_indentation :indentation_increment="properties.indentation_increment">
-                    </JSON_object><span v-if="(index + 1) !== properties.value.length">,</span>
+                    </JSON_object>
+                    <Press_key_editable placeholder="+"
+                        v-on:on_key_up="(event) => on_add_or_remove_array_element(event, index+1)">
+                    </Press_key_editable>
+                    <span v-if="(index + 1) !== properties.value.length">,</span>
                 </span>
                 <br>
             </span>
