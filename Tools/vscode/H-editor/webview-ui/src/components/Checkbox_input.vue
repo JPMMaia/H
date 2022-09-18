e20f401782f024209dfbfd088a6620f32235e372
João Maia <jpmmaia@gmail.com>
2022-09-18 12:30:52 +0300

    [vscode] Add checkbox input component

---
+++ Tools/vscode/H-editor/webview-ui/src/components/Checkbox_input.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { computed, onMounted, ref, watch } from "vue";
+
+const properties = defineProps<{
+    modelValue: boolean;
+}>();
+
+const emit = defineEmits<{
+    (e: 'update:modelValue', value: boolean): void
+}>();
+
+const input_element = ref<HTMLInputElement | null>(null);
+
+function on_input(event: InputEvent | Event): void {
+
+    if (input_element.value !== null) {
+        const value = input_element.value.checked;
+        emit('update:modelValue', value);
+    }
+}
+</script>
+
+<template>
+    <input ref="input_element" type="checkbox" :checked="properties.modelValue" @input="on_input" />
+</template>
+
+<style scoped>
+
+</style>
+
