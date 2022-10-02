7d7b78d4272dc95764be27d82a7cc1b00ce18138
João Maia <jpmmaia@gmail.com>
2022-10-02 11:19:18 +0300

    [vscode] Add Text_input component

---
+++ Tools/vscode/H-editor/webview-ui/src/components/common/Text_input.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const properties = defineProps<{
+    modelValue: string;
+}>();
+
+const emit = defineEmits<{
+    (e: 'update:modelValue', value: string): void
+}>();
+
+const input_element = ref<HTMLInputElement | null>(null);
+
+function on_input(event: InputEvent | Event): void {
+    if (input_element.value !== null) {
+        const value = input_element.value.value;
+        emit('update:modelValue', value);
+    }
+}
+</script>
+
+<template>
+    <input ref="input_element" type="text" :value="properties.modelValue" @input="on_input" />
+</template>
+
+<style scoped>
+
+</style>
+
