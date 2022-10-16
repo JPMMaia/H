0f9445071750be55fa5f39f4626f4b23b64f8fc9
João Maia <jpmmaia@gmail.com>
2022-10-16 17:54:26 +0300

    [vscode] Improve List component look

--- Tools/vscode/H-editor/webview-ui/src/components/common/List.vue
+++ Tools/vscode/H-editor/webview-ui/src/components/common/List.vue
@@ -63,30 +63,35 @@ function move_item_down(index: number): void {
 </script>
 
 <template>
-    <div>
-        <select ref="select_item_html_element" :modelValue="selected_item_index"
-            v-on:change="event => on_selected_item_index_changed(event)" :size="Math.max(properties.items.length, 3)">
-            <option v-for="(item, index) in properties.items" v-bind:key="index" :value="index">
-                <slot name="item_title" v-bind="item"></slot>
-            </option>
-        </select>
-        <div>
-            <vscode-button @click="add_item(selected_item_index !== undefined ? selected_item_index+1 : 0)">
-                <i class="codicon codicon-add"></i>
-            </vscode-button>
-            <vscode-button :disabled="selected_item_index === undefined"
-                @click="remove_item(selected_item_index !== undefined ? selected_item_index : 0)">
-                <i class="codicon codicon-remove"></i>
-            </vscode-button>
-            <vscode-button :disabled="selected_item_index === undefined || selected_item_index === 0"
-                @click="move_item_up(selected_item_index !== undefined ? selected_item_index : 0)">
-                <i class="codicon codicon-triangle-up"></i>
-            </vscode-button>
-            <vscode-button
-                :disabled="selected_item_index === undefined || (selected_item_index + 1) === properties.items.length"
-                @click="move_item_down(selected_item_index !== undefined ? selected_item_index : 0)">
-                <i class="codicon codicon-triangle-down"></i>
-            </vscode-button>
+    <div class="column_container add_margin">
+        <div class="row_container">
+            <select class="fill_grow" ref="select_item_html_element" :modelValue="selected_item_index"
+                v-on:change="event => on_selected_item_index_changed(event)"
+                :size="Math.max(properties.items.length, 3)">
+                <option v-for="(item, index) in properties.items" v-bind:key="index" :value="index">
+                    <slot name="item_title" v-bind="item"></slot>
+                </option>
+            </select>
+            <div class="column_container no_grow">
+                <vscode-button class="square_dimensions"
+                    @click="add_item(selected_item_index !== undefined ? selected_item_index+1 : 0)">
+                    <i class="codicon codicon-add"></i>
+                </vscode-button>
+                <vscode-button class="square_dimensions" :disabled="selected_item_index === undefined"
+                    @click="remove_item(selected_item_index !== undefined ? selected_item_index : 0)">
+                    <i class="codicon codicon-remove"></i>
+                </vscode-button>
+                <vscode-button class="square_dimensions"
+                    :disabled="selected_item_index === undefined || selected_item_index === 0"
+                    @click="move_item_up(selected_item_index !== undefined ? selected_item_index : 0)">
+                    <i class="codicon codicon-triangle-up"></i>
+                </vscode-button>
+                <vscode-button class="square_dimensions"
+                    :disabled="selected_item_index === undefined || (selected_item_index + 1) === properties.items.length"
+                    @click="move_item_down(selected_item_index !== undefined ? selected_item_index : 0)">
+                    <i class="codicon codicon-triangle-down"></i>
+                </vscode-button>
+            </div>
         </div>
         <div v-if="selected_item_index !== undefined">
             <slot name="item_body" v-bind="properties.items[selected_item_index]" :key="selected_item_index"></slot>
@@ -95,5 +100,32 @@ function move_item_down(index: number): void {
 </template>
 
 <style scoped>
+.add_margin {
+    margin: 1ch;
+}
 
+.row_container {
+    display: flex;
+    flex-direction: row;
+    column-gap: 1ch;
+}
+
+.column_container {
+    display: flex;
+    flex-direction: column;
+    row-gap: 1ch;
+}
+
+.no_grow {
+    flex-grow: 0;
+}
+
+.fill_grow {
+    flex-grow: 1;
+}
+
+.square_dimensions {
+    width: 5ch;
+    height: 5ch;
+}
 </style>
