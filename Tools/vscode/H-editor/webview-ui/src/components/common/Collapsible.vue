8e0a508b32c3a0a46222287e4b843bcdc993859d
João Maia <jpmmaia@gmail.com>
2022-10-20 18:53:44 +0300

    [vscode] Add Collapsible component

---
+++ Tools/vscode/H-editor/webview-ui/src/components/common/Collapsible.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import "@vscode/codicons/dist/codicon.css";
+</script>
+
+<template>
+    <details ref="details_element">
+        <summary class="horizontal_container">
+            <i class="codicon codicon-chevron-right rotate_on_open"></i>
+            <slot name="summary"></slot>
+        </summary>
+        <div class="vertical_container">
+            <slot name="content"></slot>
+        </div>
+    </details>
+</template>
+
+<style scoped>
+.horizontal_container {
+    display: flex;
+    flex-direction: row;
+}
+
+.vertical_container {
+    display: flex;
+    flex-direction: column;
+}
+
+details>summary>i.rotate_on_open {
+    transform: none;
+}
+
+details[open]>summary>i.rotate_on_open {
+    transform: rotate(90deg);
+}
+
+.animate_transform_transition {
+    transition-property: transform;
+    transition-duration: 100ms;
+    transition-timing-function: linear;
+    transition-delay: 0ms;
+}
+</style>
