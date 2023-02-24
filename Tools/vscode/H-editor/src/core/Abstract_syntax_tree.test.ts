200e948e6627978fc7ea8fe9400f3f7d4bd2b604
João Maia <jpmmaia@gmail.com>
2023-02-24 20:29:01 +0200

    [vscode] Create AST examples

--- Tools/vscode/H-editor/src/core/Abstract_syntax_tree.test.ts
+++ Tools/vscode/H-editor/src/core/Abstract_syntax_tree.test.ts
@@ -2,41 +2,11 @@ import "mocha";
 
 import * as assert from "assert";
 import * as Abstract_syntax_tree from "./Abstract_syntax_tree";
-
-function create_node(value: string, token: Abstract_syntax_tree.Token, children: Abstract_syntax_tree.Node[], relative_start: number): Abstract_syntax_tree.Node {
-    return {
-        value: value,
-        token: token,
-        children: children,
-        cache: {
-            relative_start: relative_start
-        }
-    };
-}
-
-function create_example_0(): Abstract_syntax_tree.Node {
-
-    const children_0_0: Abstract_syntax_tree.Node[] = [
-        create_node("r.0.0", Abstract_syntax_tree.Token.Expression_return, [], 4),
-        create_node("r.0.1", Abstract_syntax_tree.Token.Expression_constant, [], 10),
-    ];
-
-    const children_0_1: Abstract_syntax_tree.Node[] = [
-        create_node("r.1.0", Abstract_syntax_tree.Token.Expression_constant, [], 4),
-    ];
-
-    const children_0: Abstract_syntax_tree.Node[] = [
-        create_node("r.0", Abstract_syntax_tree.Token.Statement, children_0_0, 1),
-        create_node("r.1", Abstract_syntax_tree.Token.Statement, children_0_1, 17),
-    ];
-
-    return create_node("r", Abstract_syntax_tree.Token.Code_block, children_0, 0);
-}
-
+import * as Abstract_syntax_tree_examples from "./Abstract_syntax_tree_examples";
 
 describe("Abstract_syntax_tree.find_node_position", () => {
 
-    const root = create_example_0();
+    const root = Abstract_syntax_tree_examples.create_0();
 
     it("Finds r", () => {
         const position = Abstract_syntax_tree.find_node_position(root, 0);
@@ -101,35 +71,35 @@ describe("Abstract_syntax_tree.find_node_common_root", () => {
 describe("Abstract_syntax_tree.find_node_range", () => {
 
     it("Finds range of r.0", () => {
-        const root = create_example_0();
+        const root = Abstract_syntax_tree_examples.create_0();
         const range = Abstract_syntax_tree.find_node_range(root, -1, [0]);
         assert.equal(range.start, 1);
         assert.equal(range.end, 17);
     });
 
     it("Finds range of r.0.0", () => {
-        const root = create_example_0();
+        const root = Abstract_syntax_tree_examples.create_0();
         const range = Abstract_syntax_tree.find_node_range(root, -1, [0, 0]);
         assert.equal(range.start, 5);
         assert.equal(range.end, 11);
     });
 
     it("Finds range of r.0.1", () => {
-        const root = create_example_0();
+        const root = Abstract_syntax_tree_examples.create_0();
         const range = Abstract_syntax_tree.find_node_range(root, -1, [0, 1]);
         assert.equal(range.start, 11);
         assert.equal(range.end, 17);
     });
 
     it("Finds range of r.1", () => {
-        const root = create_example_0();
+        const root = Abstract_syntax_tree_examples.create_0();
         const range = Abstract_syntax_tree.find_node_range(root, -1, [1]);
         assert.equal(range.start, 17);
         assert.equal(range.end, -1);
     });
 
     it("Finds range of r.1.0", () => {
-        const root = create_example_0();
+        const root = Abstract_syntax_tree_examples.create_0();
         const range = Abstract_syntax_tree.find_node_range(root, -1, [1, 0]);
         assert.equal(range.start, 21);
         assert.equal(range.end, -1);
@@ -139,7 +109,7 @@ describe("Abstract_syntax_tree.find_node_range", () => {
 describe("Abstract_syntax_tree.find_nodes_of_range", () => {
 
     it("Finds nodes of range [0, 1]", () => {
-        const root = create_example_0();
+        const root = Abstract_syntax_tree_examples.create_0();
         const result = Abstract_syntax_tree.find_nodes_of_range(root, 0, 1);
 
         assert.equal(result.parent_position, undefined);
@@ -148,7 +118,7 @@ describe("Abstract_syntax_tree.find_nodes_of_range", () => {
     });
 
     it("Finds nodes of range [1, 2]", () => {
-        const root = create_example_0();
+        const root = Abstract_syntax_tree_examples.create_0();
         const result = Abstract_syntax_tree.find_nodes_of_range(root, 1, 2);
 
         assert.notEqual(result.parent_position, undefined);
@@ -160,7 +130,7 @@ describe("Abstract_syntax_tree.find_nodes_of_range", () => {
     });
 
     it("Finds nodes of range [6, 10]", () => {
-        const root = create_example_0();
+        const root = Abstract_syntax_tree_examples.create_0();
         const result = Abstract_syntax_tree.find_nodes_of_range(root, 6, 10);
 
         assert.notEqual(result.parent_position, undefined);
@@ -172,7 +142,7 @@ describe("Abstract_syntax_tree.find_nodes_of_range", () => {
     });
 
     it("Finds nodes of range [5, 11]", () => {
-        const root = create_example_0();
+        const root = Abstract_syntax_tree_examples.create_0();
         const result = Abstract_syntax_tree.find_nodes_of_range(root, 5, 11);
 
         assert.notEqual(result.parent_position, undefined);
@@ -184,7 +154,7 @@ describe("Abstract_syntax_tree.find_nodes_of_range", () => {
     });
 
     it("Finds nodes of range [10, 15]", () => {
-        const root = create_example_0();
+        const root = Abstract_syntax_tree_examples.create_0();
         const result = Abstract_syntax_tree.find_nodes_of_range(root, 10, 15);
 
         assert.notEqual(result.parent_position, undefined);
@@ -196,7 +166,7 @@ describe("Abstract_syntax_tree.find_nodes_of_range", () => {
     });
 
     it("Finds nodes of range [10, 19]", () => {
-        const root = create_example_0();
+        const root = Abstract_syntax_tree_examples.create_0();
         const result = Abstract_syntax_tree.find_nodes_of_range(root, 10, 19);
 
         assert.notEqual(result.parent_position, undefined);
@@ -208,7 +178,7 @@ describe("Abstract_syntax_tree.find_nodes_of_range", () => {
     });
 
     it("Finds nodes of range [17, 10]", () => {
-        const root = create_example_0();
+        const root = Abstract_syntax_tree_examples.create_0();
         const result = Abstract_syntax_tree.find_nodes_of_range(root, 17, 19);
 
         assert.notEqual(result.parent_position, undefined);
@@ -223,19 +193,19 @@ describe("Abstract_syntax_tree.find_nodes_of_range", () => {
 describe("Abstract_syntax_tree.find_top_level_node_position", () => {
 
     it("Finds statement r.0.0", () => {
-        const root = create_example_0();
+        const root = Abstract_syntax_tree_examples.create_0();
         const position = Abstract_syntax_tree.find_top_level_node_position(root, [0, 0]);
         assert.deepEqual(position, [0]);
     });
 
     it("Finds statement r.1", () => {
-        const root = create_example_0();
+        const root = Abstract_syntax_tree_examples.create_0();
         const position = Abstract_syntax_tree.find_top_level_node_position(root, [1]);
         assert.deepEqual(position, [1]);
     });
 
     it("Finds code block r", () => {
-        const root = create_example_0();
+        const root = Abstract_syntax_tree_examples.create_0();
         const position = Abstract_syntax_tree.find_top_level_node_position(root, []);
         assert.deepEqual(position, []);
     });
@@ -244,7 +214,7 @@ describe("Abstract_syntax_tree.find_top_level_node_position", () => {
 describe("Abstract_syntax_tree.iterate_forward_with_repetition", () => {
     it("Visits example_0 nodes correctly", () => {
 
-        const root = create_example_0();
+        const root = Abstract_syntax_tree_examples.create_0();
 
         let current_node = root;
         let current_position: number[] = [];
---
+++ Tools/vscode/H-editor/src/core/Abstract_syntax_tree_examples.ts
@@ -0,0 +1,31 @@
+import * as Abstract_syntax_tree from "./Abstract_syntax_tree";
+
+function create_node(value: string, token: Abstract_syntax_tree.Token, children: Abstract_syntax_tree.Node[], relative_start: number): Abstract_syntax_tree.Node {
+    return {
+        value: value,
+        token: token,
+        children: children,
+        cache: {
+            relative_start: relative_start
+        }
+    };
+}
+
+export function create_0(): Abstract_syntax_tree.Node {
+
+    const children_0_0: Abstract_syntax_tree.Node[] = [
+        create_node("r.0.0", Abstract_syntax_tree.Token.Expression_return, [], 4),
+        create_node("r.0.1", Abstract_syntax_tree.Token.Expression_constant, [], 10),
+    ];
+
+    const children_0_1: Abstract_syntax_tree.Node[] = [
+        create_node("r.1.0", Abstract_syntax_tree.Token.Expression_constant, [], 4),
+    ];
+
+    const children_0: Abstract_syntax_tree.Node[] = [
+        create_node("r.0", Abstract_syntax_tree.Token.Statement, children_0_0, 1),
+        create_node("r.1", Abstract_syntax_tree.Token.Statement, children_0_1, 17),
+    ];
+
+    return create_node("r", Abstract_syntax_tree.Token.Code_block, children_0, 0);
+}
