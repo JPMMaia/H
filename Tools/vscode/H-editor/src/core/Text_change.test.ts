b443bb5d13791d760a399c877a0576bc6a09a7eb
JPMMaia <jpmmaia@gmail.com>
2023-08-23 20:50:54 +0300

    [vscode] Add draft implementation of Text_change.update()

--- Tools/vscode/H-editor/src/core/Text_change.test.ts
+++ Tools/vscode/H-editor/src/core/Text_change.test.ts
@@ -3,28 +3,46 @@ import "mocha";
 import * as assert from "assert";
 import * as Abstract_syntax_tree_from_module from "./Abstract_syntax_tree_from_module";
 import * as Default_grammar from "./Default_grammar";
+import * as Grammar from "./Grammar";
 import * as Module_examples from "./Module_examples";
+import * as Parse_tree_convertor from "./Parse_tree_convertor";
+import * as Language from "./Language";
 import * as Symbol_database from "./Symbol_database";
 import * as Text_change from "./Text_change";
 
 describe("Text_change.update", () => {
 
+    const language_description = Language.create_default_description();
+
     const module = Module_examples.create_empty();
     const symbol_database = Symbol_database.create_edit_database(module);
-    const grammar = Default_grammar.create_grammar();
-    const abstract_syntax_tree = Abstract_syntax_tree_from_module.create_module_node(module, symbol_database, grammar);
+    const declarations = Parse_tree_convertor.create_declarations(module);
+    const initial_parse_tree = Parse_tree_convertor.module_to_parse_tree(module, symbol_database, declarations, language_description.production_rules);
 
     it("Handles add first character", () => {
 
-        const text_after_change = "f";
-        const change: Text_change.Change = {
-            range_offset: 0,
-            range_length: 0,
-            new_text: "f"
+        const document_state: Text_change.Document_state = {
+            text: "",
+            parse_tree: initial_parse_tree,
+            module: module,
+            symbol_database: symbol_database,
+            declarations: declarations
         };
 
-        Text_change.update(text_after_change, change, module, symbol_database, grammar, abstract_syntax_tree);
+        const text_changes: Text_change.Text_change[] = [
+            {
+                range: {
+                    start: { line: 0, character: 0 },
+                    end: { line: 0, character: 0 },
+                },
+                text: "m"
+            }
+        ];
 
-
+        Text_change.update(
+            language_description,
+            document_state,
+            text_changes
+        );
     });
 });
--- Tools/vscode/H-editor/src/core/Text_change.ts
+++ Tools/vscode/H-editor/src/core/Text_change.ts
@@ -1,21 +1,104 @@
-import * as Abstract_syntax_tree from "./Abstract_syntax_tree";
 import * as Core from "../utilities/coreModelInterface";
+import * as Module_change_update from "../utilities/Change_update";
+import * as Language from "./Language";
+import * as Parser from "./Parser";
+import * as Parser_node from "./Parser_node";
+import * as Parse_tree_convertor from "./Parse_tree_convertor";
+import { scan_new_change } from "./Scan_new_changes";
 import * as Symbol_database from "./Symbol_database";
-import * as Grammar from "./Grammar";
 
-export interface Change {
-    range_offset: number;
-    range_length: number;
-    new_text: string;
+export interface Text_position {
+    line: number;
+    character: number;
+}
+
+export interface Text_range {
+    start: Text_position;
+    end: Text_position;
+}
+
+export interface Text_change {
+    range: Text_range;
+    text: string;
+}
+
+export interface Document_state {
+    text: string;
+    parse_tree: Parser_node.Node;
+    module: Core.Module;
+    symbol_database: Symbol_database.Edit_module_database;
+    declarations: Parse_tree_convertor.Declaration[];
 }
 
 export function update(
-    text_after_change: string,
-    change: Change,
-    module: Core.Module,
-    symbol_database: Symbol_database.Edit_module_database,
-    grammar: Grammar.Grammar,
-    abstract_syntax_tree: Abstract_syntax_tree.Node
-): void {
+    language_description: Language.Description,
+    state: Document_state,
+    text_changes: Text_change[]
+): Document_state {
 
+    // TODO handle other text changes
+    const text_change = text_changes[0];
+
+    const parse_tree_root = state.parse_tree;
+
+    const scanned_input_change = scan_new_change(
+        parse_tree_root,
+        { line: text_change.range.start.line, column: text_change.range.start.character },
+        { line: text_change.range.end.line, column: text_change.range.end.character },
+        text_change.text
+    );
+
+    if (scanned_input_change.new_words.length > 0) {
+
+        const start_change_node_position = scanned_input_change.start_change !== undefined ? scanned_input_change.start_change.position : [];
+        const after_change_node_position = scanned_input_change.after_change !== undefined ? scanned_input_change.after_change.position : [];
+
+        const parse_result = Parser.parse_incrementally(
+            parse_tree_root,
+            start_change_node_position,
+            scanned_input_change.new_words,
+            after_change_node_position,
+            language_description.actions_table,
+            language_description.go_to_table,
+            language_description.array_infos,
+            language_description.map_word_to_terminal
+        );
+
+        // TODO figure out errors
+
+        if (parse_result.status === Parser.Parse_status.Accept) {
+            // TODO can be cached
+            const production_rule_to_value_map = Parse_tree_convertor.create_production_rule_to_value_map(language_description.production_rules);
+            const production_rule_to_change_action_map = Parse_tree_convertor.create_production_rule_to_change_action_map(language_description.production_rules);
+
+            const module_changes = Parse_tree_convertor.create_module_changes(
+                state.module,
+                state.symbol_database,
+                state.declarations,
+                language_description.production_rules,
+                production_rule_to_value_map,
+                production_rule_to_change_action_map,
+                state.parse_tree,
+                parse_result.changes
+            );
+
+            // TODO Create symbol changes ?
+
+            Parser.apply_changes(parse_tree_root, parse_result.changes);
+
+            // TODO Apply module changes
+            /*const module_pointer = {
+                value: module
+            };
+            Module_change_update.update_object_with_change(module_pointer, module_changes);*/
+
+            // TODO Apply symbol changes
+
+            // TODO update parse tree text position
+
+            // TODO update new text
+        }
+    }
+
+    return state; // TODO also return changes that were not applied?
 }
