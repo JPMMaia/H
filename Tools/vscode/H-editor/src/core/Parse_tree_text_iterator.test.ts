d7b5cb60eb1e43208fd5a2bba3ade5771a15ab6d
JPMMaia <jpmmaia@gmail.com>
2023-08-30 19:40:46 +0300

    [vscode] Add Parse_tree_text_iterator

---
+++ Tools/vscode/H-editor/src/core/Parse_tree_text_iterator.test.ts
@@ -0,0 +1,133 @@
+import "mocha";
+
+import * as assert from "assert";
+import * as Grammar from "./Grammar";
+import * as Parser_node from "./Parser_node";
+import * as Parse_tree_text_iterator from "./Parse_tree_text_iterator";
+
+function create_parse_node(value: string, production_rule_index: number | undefined, children: Parser_node.Node[]): Parser_node.Node {
+    return {
+        word: { value: value, type: Grammar.Word_type.Alphanumeric },
+        state: -1,
+        production_rule_index: production_rule_index,
+        children: children,
+        text_position: {
+            line: 0,
+            column: 0
+        }
+    };
+}
+
+function create_test_parse_tree(): Parser_node.Node {
+    const root = create_parse_node(
+        "S", 0,
+        [
+            create_parse_node("Declaration", 1, [
+                create_parse_node("Function", 2, [
+                    create_parse_node("Function_declaration", 3, [
+                        create_parse_node("function", undefined, []),
+                        create_parse_node("name_0", undefined, []),
+                        create_parse_node("(", undefined, []),
+                        create_parse_node(")", undefined, []),
+                        create_parse_node("->", undefined, []),
+                        create_parse_node("(", undefined, []),
+                        create_parse_node(")", undefined, []),
+                    ]),
+                    create_parse_node("Function_definition", 4, [
+                        create_parse_node("{", undefined, []),
+                        create_parse_node("}", undefined, []),
+                    ]),
+                ]),
+            ])
+        ]
+    );
+
+    return root;
+}
+
+function create_test_text(): string {
+    const text = `
+        function name_0() -> ()
+        {
+        }
+    `;
+
+    return text;
+}
+
+describe("Parse_tree_text_iterator", () => {
+
+    const root = create_test_parse_tree();
+    const text = create_test_text();
+
+    const expected_word: string[] = [
+        "function",
+        "name_0",
+        "(",
+        ")",
+        "->",
+        "(",
+        ")",
+        "{",
+        "}"
+    ];
+
+    const first_word_offset = text.indexOf("function");
+
+    const expected_offset: number[] = [
+        first_word_offset,
+        first_word_offset + 9,
+        first_word_offset + 15,
+        first_word_offset + 16,
+        first_word_offset + 18,
+        first_word_offset + 21,
+        first_word_offset + 22,
+        text.indexOf("{"),
+        text.indexOf("}"),
+    ];
+
+    it("Can iterate forward", () => {
+
+        let iterator = Parse_tree_text_iterator.begin(root, text);
+
+        assert.notEqual(iterator.node, undefined);
+        if (iterator.node !== undefined) {
+            assert.equal(iterator.node.word.value, expected_word[0]);
+            assert.equal(iterator.offset, expected_offset[0]);
+        }
+
+        for (let index = 1; index < expected_word.length; ++index) {
+
+            iterator = Parse_tree_text_iterator.next(iterator);
+
+            assert.notEqual(iterator.node, undefined);
+            if (iterator.node !== undefined) {
+                assert.equal(iterator.node.word.value, expected_word[index]);
+                assert.equal(iterator.offset, expected_offset[index]);
+            }
+        }
+
+        iterator = Parse_tree_text_iterator.next(iterator);
+        assert.equal(iterator.node, undefined);
+        assert.equal(iterator.offset, text.length);
+    });
+
+    it("Can iterate back", () => {
+
+        let iterator = Parse_tree_text_iterator.end(root, text);
+
+        assert.equal(iterator.node, undefined);
+        assert.equal(iterator.offset, text.length);
+
+        for (let index = expected_offset.length - 1; index >= 0; --index) {
+
+            iterator = Parse_tree_text_iterator.previous(iterator);
+
+            assert.notEqual(iterator.node, undefined);
+            if (iterator.node !== undefined) {
+                assert.equal(iterator.node.word.value, expected_word[index]);
+                assert.equal(iterator.offset, expected_offset[index]);
+            }
+        }
+    });
+});
---
+++ Tools/vscode/H-editor/src/core/Parse_tree_text_iterator.ts
@@ -0,0 +1,135 @@
+import * as Parser_node from "./Parser_node";
+import * as Scanner from "./Scanner";
+
+export interface Iterator {
+    root: Parser_node.Node;
+    text: string;
+    node: Parser_node.Node | undefined;
+    node_position: number[];
+    offset: number;
+}
+
+function is_terminal_node_with_text(node: Parser_node.Node, position: number[]): boolean {
+    return node.children.length === 0 && node.production_rule_index === undefined && node.word.value.length > 0;
+}
+
+export function begin(root: Parser_node.Node, text: string): Iterator {
+    const next = Parser_node.get_next_node_with_condition(root, root, [], is_terminal_node_with_text);
+
+    if (next === undefined) {
+        return end(root, text);
+    }
+
+    const offset = Scanner.ignore_whitespace_or_new_lines(text, 0);
+
+    return {
+        root: root,
+        text: text,
+        node: next.node,
+        node_position: next.position,
+        offset: offset
+    };
+}
+
+export function end(root: Parser_node.Node, text: string): Iterator {
+    return {
+        root: root,
+        text: text,
+        node: undefined,
+        node_position: [],
+        offset: text.length
+    };
+}
+
+function go_to_next_word(text: string, current_word_offset: number, current_word_length: number): number {
+    let current_offset = current_word_offset + current_word_length;
+    current_offset += Scanner.ignore_whitespace_or_new_lines(text, current_offset);
+    return current_offset;
+}
+
+export function next(iterator: Iterator): Iterator {
+
+    if (iterator.node === undefined) {
+        return end(iterator.root, iterator.text);
+    }
+
+    const next_offset = go_to_next_word(iterator.text, iterator.offset, iterator.node.word.value.length);
+
+    const next = Parser_node.get_next_node_with_condition(iterator.root, iterator.node, iterator.node_position, is_terminal_node_with_text);
+
+    if (next === undefined) {
+        return end(iterator.root, iterator.text);
+    }
+
+    return {
+        root: iterator.root,
+        text: iterator.text,
+        node: next.node,
+        node_position: next.position,
+        offset: next_offset
+    };
+}
+
+function go_to_previous_word(text: string, current_offset: number, previous_node_length: number): number {
+
+    for (let index = current_offset; index > 0; --index) {
+        const character_index = index - 1;
+        const character = text[character_index];
+        if (!Scanner.is_whitespace_or_new_line(character)) {
+            return index - previous_node_length;
+        }
+    }
+
+    return 0;
+}
+
+export function previous(iterator: Iterator): Iterator {
+
+    if (iterator.node === undefined) {
+        const rightmost_descendant = Parser_node.get_rightmost_descendant(iterator.root, []);
+
+        if (is_terminal_node_with_text(rightmost_descendant.node, rightmost_descendant.position)) {
+
+            const previous_offset = go_to_previous_word(iterator.text, iterator.text.length, rightmost_descendant.node.word.value.length);
+
+            return {
+                root: iterator.root,
+                text: iterator.text,
+                node: rightmost_descendant.node,
+                node_position: rightmost_descendant.position,
+                offset: previous_offset
+            };
+        }
+
+        const previous = Parser_node.get_previous_node_with_condition(iterator.root, rightmost_descendant.node, rightmost_descendant.position, is_terminal_node_with_text);
+
+        if (previous === undefined) {
+            return end(iterator.root, iterator.text);
+        }
+
+        const previous_offset = go_to_previous_word(iterator.text, iterator.offset, previous.node.word.value.length);
+
+        return {
+            root: iterator.root,
+            text: iterator.text,
+            node: previous.node,
+            node_position: previous.position,
+            offset: previous_offset
+        };
+    }
+
+    const previous = Parser_node.get_previous_node_with_condition(iterator.root, iterator.node, iterator.node_position, is_terminal_node_with_text);
+    if (previous === undefined) {
+        return end(iterator.root, iterator.text);
+    }
+
+    const previous_offset = go_to_previous_word(iterator.text, iterator.offset, previous.node.word.value.length);
+
+    return {
+        root: iterator.root,
+        text: iterator.text,
+        node: previous.node,
+        node_position: previous.position,
+        offset: previous_offset
+    };
+}
