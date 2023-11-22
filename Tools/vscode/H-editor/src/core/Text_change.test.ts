f764fa96299c2cae771125c961e94586c94c8ff1
JPMMaia <jpmmaia@gmail.com>
2023-11-22 22:00:06 +0200

    [vscode] Add new Text_change test

--- Tools/vscode/H-editor/src/core/Text_change.test.ts
+++ Tools/vscode/H-editor/src/core/Text_change.test.ts
@@ -1,6 +1,7 @@
 import "mocha";
 
 import * as assert from "assert";
+import * as Core from "./Core_interface";
 import * as Document from "./Document";
 import * as Language from "./Language";
 import * as Text_change from "./Text_change";
@@ -489,6 +490,91 @@ describe("Text_change.update", () => {
 
             const expected_type = Type_utilities.parse_type_name("Float16");
             assert.deepEqual(new_alias.type.elements, expected_type);
+        }
+    });
+
+    it("Handles adding first function parameter", () => {
+
+        const document_state = Document.create_empty_state(language_description.production_rules);
+
+        {
+            const text_changes: Text_change.Text_change[] = [
+                {
+                    range: {
+                        start: 0,
+                        end: 0,
+                    },
+                    text: "module Foo;\n\nexport function My_function() -> () {}\n"
+                }
+            ];
+
+            const text_after_changes = "module Foo;\n\nexport function My_function() -> () {}\n";
+
+            Text_change.update(
+                language_description,
+                document_state,
+                text_changes,
+                text_after_changes
+            );
+
+            assert.equal(document_state.module.name, "Foo");
+
+            assert.equal(document_state.pending_text_changes.length, 0);
+
+            assert.equal(document_state.module.export_declarations.function_declarations.size, 1);
+            assert.equal(document_state.module.export_declarations.function_declarations.elements.length, 1);
+
+            const new_function_declaration = document_state.module.export_declarations.function_declarations.elements[0];
+            assert.equal(new_function_declaration.name, "My_function");
+            assert.equal(new_function_declaration.linkage, Core.Linkage.External);
+            assert.equal(new_function_declaration.input_parameter_names.size, 0);
+            assert.equal(new_function_declaration.input_parameter_names.elements.length, 0);
+            assert.equal(new_function_declaration.output_parameter_names.size, 0);
+            assert.equal(new_function_declaration.output_parameter_names.elements.length, 0);
+            assert.equal(new_function_declaration.type.is_variadic, false);
+            assert.equal(new_function_declaration.type.input_parameter_types.size, 0);
+            assert.equal(new_function_declaration.type.input_parameter_types.elements.length, 0);
+            assert.equal(new_function_declaration.type.output_parameter_types.size, 0);
+            assert.equal(new_function_declaration.type.output_parameter_types.elements.length, 0);
+        }
+
+        {
+            const text_changes: Text_change.Text_change[] = [
+                {
+                    range: {
+                        start: 41,
+                        end: 41,
+                    },
+                    text: "first: Float16"
+                }
+            ];
+
+            const text_after_changes = "module Foo;\n\nexport function My_function() -> (first: Float16) {}\n";
+
+            Text_change.update(
+                language_description,
+                document_state,
+                text_changes,
+                text_after_changes
+            );
+
+            assert.equal(document_state.module.name, "Foo");
+
+            assert.equal(document_state.pending_text_changes.length, 0);
+
+            assert.equal(document_state.module.export_declarations.function_declarations.size, 1);
+            assert.equal(document_state.module.export_declarations.function_declarations.elements.length, 1);
+
+            const new_function_declaration = document_state.module.export_declarations.function_declarations.elements[0];
+            assert.equal(new_function_declaration.input_parameter_names.size, 1);
+            assert.equal(new_function_declaration.input_parameter_names.elements.length, 1);
+            assert.equal(new_function_declaration.type.input_parameter_types.size, 1);
+            assert.equal(new_function_declaration.type.input_parameter_types.elements.length, 1);
+            if (new_function_declaration.input_parameter_names.elements.length > 0 && new_function_declaration.type.input_parameter_types.elements.length > 0) {
+                assert.equal(new_function_declaration.input_parameter_names.elements[0], "first");
+                const expected_type = Type_utilities.parse_type_name("Float16");
+                assert.deepEqual(new_function_declaration.type.input_parameter_types.elements[0], expected_type[0]);
+            }
         }
     });
 });
