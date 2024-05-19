7c4003457ba8d683026aaba5988d8848e6f1208a
JPMMaia <jpmmaia@gmail.com>
2024-05-19 19:27:00 +0300

    Add windows default library search path

---
+++ Source/Common/Filesystem_common.cpp
@@ -0,0 +1,30 @@
+module;
+
+#include <filesystem>
+#include <optional>
+#include <span>
+#include <string_view>
+
+module h.common.filesystem_common;
+
+namespace h::common
+{
+    std::optional<std::filesystem::path> search_file(
+        std::string_view const filename,
+        std::span<std::filesystem::path const> const search_paths
+    )
+    {
+        for (std::filesystem::path const& search_path : search_paths)
+        {
+            for (const std::filesystem::directory_entry& entry : std::filesystem::recursive_directory_iterator{ search_path })
+            {
+                if (entry.path().filename() == filename)
+                {
+                    return entry.path();
+                }
+            }
+        }
+
+        return std::nullopt;
+    }
+}
