eb97ef256bbdf5bf9e6fd3e33c845e2304a09f02
JPMMaia <jpmmaia@gmail.com>
2024-12-14 15:18:41 +0200

    Add support for absolute file paths when searching for files

--- Source/Compiler/Project/Artifact.cpp
+++ Source/Compiler/Project/Artifact.cpp
@@ -406,6 +406,12 @@ namespace h::compiler
         std::span<std::filesystem::path const> const search_paths
     )
     {
+        {
+            std::filesystem::path const file_path = filename;
+            if (file_path.is_absolute())
+                return file_path;
+        }
+
         for (std::filesystem::path const& search_path : search_paths)
         {
             for (const std::filesystem::directory_entry& entry : std::filesystem::recursive_directory_iterator{ search_path })
