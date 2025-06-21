f29b925c32b8a85553341bf6b3e2949a2eea3eab
JPMMaia <jpmmaia@gmail.com>
2025-06-21 10:23:54 +0300

    Handle language client and server capabilities

--- Source/Language_server/Message_handler.cpp
+++ Source/Language_server/Message_handler.cpp
@@ -39,23 +39,67 @@ namespace h::language_server
 
         lsp::Connection connection{socket};
         lsp::MessageHandler message_handler{connection};
+
+        bool has_workspace_folder_capability = false;
         
         message_handler.add<lsp::requests::Initialize>(
-            [](lsp::requests::Initialize::Params&& params) -> lsp::requests::Initialize::Result
+            [&](lsp::requests::Initialize::Params&& parameters) -> lsp::requests::Initialize::Result
             {
-               lsp::requests::Initialize::Result result
-               {
+                lsp::ClientCapabilities const& client_capabilities = parameters.capabilities;
+
+                if (client_capabilities.workspace)
+                {
+                    has_workspace_folder_capability = client_capabilities.workspace->workspaceFolders.value_or(false);
+                }
+
+                lsp::ServerCapabilitiesWorkspace workspace_server_capabilities
+                {
+                    .workspaceFolders = lsp::WorkspaceFoldersServerCapabilities
+                    {
+                        .supported = true,
+                        .changeNotifications = true,
+                    }
+                };
+
+                lsp::TextDocumentSyncOptions text_document_sync_server_capabilities
+                {
+                    .openClose = true,
+                    .change = lsp::TextDocumentSyncKind::Incremental,
+                    .willSave = false,
+                    .willSaveWaitUntil = false,
+                    .save = false,
+                };
+
+                lsp::requests::Initialize::Result result
+                {
                     .capabilities =
                     {
+                        .textDocumentSync = text_document_sync_server_capabilities,
+                        .workspace = workspace_server_capabilities,
                     },
                     .serverInfo = lsp::InitializeResultServerInfo
                     {
                         .name = "Hlang Language Server",
                         .version = "0.1.0"
                     }
-               };
+                };
 
                return result;
+            }
+        );
+
+        message_handler.add<lsp::notifications::Initialized>(
+            [&](lsp::notifications::Initialized::Params&& parameters) -> void
+            {
+                if (has_workspace_folder_capability)
+                {
+                    message_handler.add<lsp::notifications::Workspace_DidChangeWorkspaceFolders>(
+                        [&](lsp::notifications::Workspace_DidChangeWorkspaceFolders::Params&& parameters) -> void
+                        {
+                            connection.writeMessage("Workspace folder change event received.");
+                        }
+                    );
+                }
             }
         );
 
