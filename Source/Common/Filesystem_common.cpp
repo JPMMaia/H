293894e1fd1c814bbe61af9554aa03a2e1dac5ce
JPMMaia <jpmmaia@gmail.com>
2025-06-23 10:36:05 +0300

    Add search_files

--- Source/Common/Filesystem_common.cpp
+++ Source/Common/Filesystem_common.cpp
@@ -33,4 +33,30 @@ namespace h::common
 
         return std::nullopt;
     }
+
+    std::pmr::vector<std::filesystem::path> search_files(
+        std::filesystem::path const& root_directory,
+        std::string_view const filename,
+        std::pmr::polymorphic_allocator<> const& temporaries_allocator,
+        std::pmr::polymorphic_allocator<> const& output_allocator
+    )
+    {
+        std::pmr::vector<std::filesystem::path> found_files{temporaries_allocator};
+
+        for (const std::filesystem::directory_entry& entry : std::filesystem::recursive_directory_iterator{ root_directory })
+        {
+            if (entry.is_regular_file())
+            {
+                std::filesystem::path const& entry_path = entry.path();
+                std::filesystem::path const filename_path = entry_path.filename();
+
+                if (filename_path.generic_string() == filename)
+                {
+                    found_files.push_back(entry_path);
+                }
+            }
+        }
+
+        return std::pmr::vector<std::filesystem::path>{std::move(found_files), output_allocator};
+    }
 }
--- Source/Common/Filesystem_common.cppm
+++ Source/Common/Filesystem_common.cppm
@@ -1,9 +1,11 @@
 module;
 
 #include <filesystem>
+#include <memory_resource>
 #include <optional>
 #include <span>
 #include <string_view>
+#include <vector>
 
 export module h.common.filesystem_common;
 
@@ -12,5 +14,12 @@ namespace h::common
     export std::optional<std::filesystem::path> search_file(
         std::string_view const filename,
         std::span<std::filesystem::path const> const search_paths
+    );
+
+    export std::pmr::vector<std::filesystem::path> search_files(
+        std::filesystem::path const& root_directory,
+        std::string_view const filename,
+        std::pmr::polymorphic_allocator<> const& temporaries_allocator,
+        std::pmr::polymorphic_allocator<> const& output_allocator
     );
 }
